f45404107f19c956d11a7be25c4614f8204845d3
YoussefMohamedCs <144650377+YoussefMohamedCs@users.noreply.github.com>
2023-10-27 17:08:08 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let inputField = document.getElementById("inputField")
+let Button = document.getElementById("button")
+let loading = document.getElementById("loading")
+let skchase = document.getElementById("skchase")
+let h1= document.getElementById("h1")
+
+Button.addEventListener("click" , function(e){
+    let x = `https://codeforces.com/profile/${inputField.value}`
+    if(inputField.value == ""){
+        alert("ohhh fuck you add fucken username")
+    }else{
+    Button.href = x;
+    }
+
+ 
+})
+
+setTimeout(function(){
+    skchase.style.display="none"
+
+} , 5000)
+
+setTimeout(function(){
+h1.style.display="flex"
+
+} , 5001)
+
+setTimeout(function(){
+    loading.style.display="none"
+    } , 7000)
+
+
+
+
